371eabeae85cf060a0279da35fe483255f613d42
Daniel Lomelino <dlomelino@gmail.com>
2019-11-04 10:34:15 -0500

    Players filter UI in place

--- src/components/PlayersFilterBar.vue
+++ src/components/PlayersFilterBar.vue
@@ -12,29 +12,104 @@
           <v-row no-gutters>
             <v-col sm="4">
               <v-switch
-                v-model="matchType"
-                :label="matchType.toString()"
+                v-model="filterType"
+                :label="filterType.toString()"
                 true-value="doubles"
                 false-value="singles"
               ></v-switch>
             </v-col>
-            <v-col sm="4">two</v-col>
+            <v-col sm="4">
+              <v-radio-group v-model="filterFormat">
+                <v-radio
+                  label="mixed"
+                  value="mixed"
+                />
+                <v-radio
+                  label="men"
+                  value="men"
+                />
+                <v-radio
+                  label="women"
+                  value="women"
+                />
+              </v-radio-group>
+            </v-col>
             <v-col sm="4">
               <v-row no-gutters>
-                <v-col cols="6">From</v-col>
-                <v-col cols="6">To</v-col>
+                <v-col cols="4">
+                  <v-menu
+                    ref="fromDateMenu"
+                    :close-on-content-click="false"
+                    :return-value.sync="dateRange.from"
+                    offset-y
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="dateRange.from"
+                        label="From date"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="date"
+                      no-title
+                      scrollable
+                    >
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        text
+                        color="primary"
+                        @click="$refs.fromDateMenu.isActive = false"
+                      >Cancel</v-btn>
+                      <v-btn
+                        text
+                        color="primary"
+                        @click="$refs.fromDateMenu.save(date)"
+                      >OK</v-btn>
+                    </v-date-picker>
+                  </v-menu>
+                </v-col>
+              </v-row>
+              <v-row no-gutters>
+                <v-col cols="4">
+                  <v-menu
+                    ref="toDateMenu"
+                    :close-on-content-click="false"
+                    :return-value.sync="dateRange.to"
+                    offset-y
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="dateRange.to"
+                        label="To date"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="date"
+                      no-title
+                      scrollable
+                    >
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        text
+                        color="primary"
+                        @click="$refs.toDateMenu.isActive = false"
+                      >Cancel</v-btn>
+                      <v-btn
+                        text
+                        color="primary"
+                        @click="$refs.toDateMenu.save(date)"
+                      >OK</v-btn>
+                    </v-date-picker>
+                  </v-menu>
+                </v-col>
               </v-row>
             </v-col>
-          </v-row>
-          <v-row no-gutters>
-            <v-col sm="4">Rating</v-col>
-            <v-col sm="4">Surface</v-col>
-            <v-col sm="4">Setting</v-col>
-          </v-row>
-          <v-row no-gutters>
-            <v-col sm="4">four</v-col>
-            <v-col sm="4">five</v-col>
-            <v-col sm="4">six</v-col>
           </v-row>
         </v-container>
       </v-expansion-panel-content>
@@ -47,7 +122,13 @@ export default {
   name: 'playersFilterBar',
   data () {
     return {
-      matchType: 'singles'
+      date: null,
+      dateRange: {
+        from: null,
+        to: null
+      },
+      filterFormat: 'mixed',
+      filterType: 'doubles'
     }
   }
 }
