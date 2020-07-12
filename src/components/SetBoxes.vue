38b49f406bb10366c8bcd6e6c80918f02bf3fb0e
Daniel Lomelino <dlomelino@gmail.com>
2020-07-11 22:44:21 -0400

    UI update for entering tiebreaker scores
    Move Set input row to its own component
    Reuse Set input row as Tiebreaker inputs

---
+++ src/components/SetBoxes.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-col class="py-0 my-auto ml-2">
+    <v-row no-gutters>
+      <v-col
+        cols="2"
+        v-for="n in numSets" :key="n"
+        class="pa-0 mr-1"
+      >
+        <v-text-field
+          type="number"
+          min="0"
+          outlined
+          flat
+          dense
+          hide-details
+          solo
+          @input="(score) => onChangeSet(n, score)"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'setBoxes',
+  props: [
+    'numSets',
+    'onChangeSet'
+  ]
+}
+</script>
