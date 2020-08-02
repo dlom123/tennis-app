6f21684fbff28176ca54867db7809240692b52cd
Daniel Lomelino <dlomelino@gmail.com>
2020-08-01 21:18:06 -0400

    Get port number from env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
-const port = process.env.PORT || 80
+const port = process.env.VUE_APP_PORT || 80
 
 app.use(serveStatic(path.join(__dirname, 'dist')))
 app.listen(port)
